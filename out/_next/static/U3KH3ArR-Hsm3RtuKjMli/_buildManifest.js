e6aefa5928c4e86adecad3a287e50c91a74de10f
Sachin Yadav <webdevsachinyadav@gmail.com>
2025-04-15 19:26:46 +0530

    Add files via upload

---
+++ out/_next/static/U3KH3ArR-Hsm3RtuKjMli/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-dc4699fac8afe109.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
